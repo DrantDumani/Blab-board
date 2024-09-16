dfd13f6d112d8e8cdd51d5935a2c69620cf97789
Darnell <darnjames@gmail.com>
2024-09-16 11:50:10 -0400

    Redirect route based on token

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,6 +1,7 @@
 import { SignUpForm } from "../../components/SignUpForm/SignUpForm";
 import { LoginForm } from "../../components/LoginForm/LoginForm";
 import { useState } from "react";
+import { Navigate } from "react-router-dom";
 
 export function Auth() {
   const [formToggle, setFormToggle] = useState("signup");
@@ -8,19 +9,27 @@ export function Auth() {
   const onToggleForm = () =>
     setFormToggle((t) => (t === "signup" ? "login" : "signup"));
 
+  const token = localStorage.getItem("token");
+
   return (
-    <div>
-      {formToggle === "signup" ? <SignUpForm /> : <LoginForm />}
-      {formToggle === "signup" ? (
-        <p>
-          Already have an account? Click here to{" "}
-          <button onClick={onToggleForm}>Login</button>
-        </p>
+    <>
+      {!token ? (
+        <div>
+          {formToggle === "signup" ? <SignUpForm /> : <LoginForm />}
+          {formToggle === "signup" ? (
+            <p>
+              Already have an account? Click here to{" "}
+              <button onClick={onToggleForm}>Login</button>
+            </p>
+          ) : (
+            <p>
+              Click here to <button onClick={onToggleForm}>Sign Up</button>
+            </p>
+          )}
+        </div>
       ) : (
-        <p>
-          Click here to <button onClick={onToggleForm}>Sign Up</button>
-        </p>
+        <Navigate to={"/dashboard"} />
       )}
-    </div>
+    </>
   );
 }
--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,5 +1,6 @@
 export function Layout() {
   const token = localStorage.getItem("token");
-  const payload = token && JSON.parse(window.atob(token.split(".")[1]));
-  return <h1>Placeholder for layout page. Welcome {payload.username}</h1>;
+  const user = JSON.parse(window.atob(token.split(".")[1]));
+
+  return <h1>Placeholder for layout page. Welcome {user.username}</h1>;
 }
