c231ac10dbd37ab22c3e5b5c59e368f8877ae50c
Darnell <darnjames@gmail.com>
2024-10-17 17:21:16 -0400

    Add loading screen when submitting a form

--- src/components/Form/AuthForm.jsx
+++ src/components/Form/AuthForm.jsx
@@ -1,6 +1,7 @@
 import { useFetcher } from "react-router-dom";
 import PropTypes from "prop-types";
 import styles from "./AuthForm.module.css";
+import { Loading } from "../Loading/Loading";
 
 export function AuthForm({
   children,
@@ -13,23 +14,26 @@ export function AuthForm({
   const fetcher = useFetcher();
   const err = fetcher.data;
   return (
-    <fetcher.Form
-      name={name}
-      className={styles.authForm}
-      method="POST"
-      encType={enctype}
-    >
-      {children}
-      <button
-        disabled={!canSubmit}
-        className={styles.submitAuth}
-        name="intent"
-        value={intent}
+    <>
+      <fetcher.Form
+        name={name}
+        className={styles.authForm}
+        method="POST"
+        encType={enctype}
       >
-        {btnText}
-      </button>
-      {err && <p className={styles.serverErr}>{err}</p>}
-    </fetcher.Form>
+        {children}
+        <button
+          disabled={!canSubmit}
+          className={styles.submitAuth}
+          name="intent"
+          value={intent}
+        >
+          {btnText}
+        </button>
+        {err && <p className={styles.serverErr}>{err}</p>}
+      </fetcher.Form>
+      {fetcher.state === "submitting" && <Loading />}
+    </>
   );
 }
 
